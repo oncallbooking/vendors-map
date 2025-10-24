c33ed262cf0b1d9fc4a0b29c603bc9321cbc411c
oncallbooking <143792222+oncallbooking@users.noreply.github.com>
2025-10-24 07:02:35 +0530

    Create tailwind.config.cjs

---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#8b5cf6',
+        accent: '#06b6d4'
+      },
+      backdropBlur: {
+        xs: '2px'
+      }
+    }
+  },
+  plugins: []
+}
